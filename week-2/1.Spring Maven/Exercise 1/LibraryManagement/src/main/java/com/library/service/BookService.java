1cbf1eee7e5839ed97652b8af5a365edeb4978cf
Jagadeesh <jagadeesh58.b@gmail.com>
2026-07-16 17:46:19 +0530

    Remove Week-1, Week-2, Week-3 and Week-4

--- week-2/1.Spring Maven/Exercise 1/LibraryManagement/src/main/java/com/library/service/BookService.java
+++
@@ -1,7 +0,0 @@
-package com.library.service;
-
-public class BookService {
-    public void manageBooks() {
-        System.out.println("[BookService] BookService is executing library management operations...");
-    }
-}
--- week-2/1.Spring Maven/Exercise 7/LibraryManagement/src/main/java/com/library/repository/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package com.library.repository;
-
-public class BookRepository {
-    public void fetchCatalog() {
-        System.out.println("[BookRepository] Fetching catalog: ['Refactoring', 'Clean Architecture']");
-    }
-}
